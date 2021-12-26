af5a986b6cb0f609579910e194b5a0877f19d274
Obaidul Nasir <83164409+obaidulnasir@users.noreply.github.com>
2021-12-26 15:20:25 +0600

    firebase setup

---
+++ src/components/hooks/useFirebase.js
@@ -0,0 +1,143 @@
+import { useEffect, useState } from "react";
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signOut,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+} from "firebase/auth";
+import initializeAuthentication from "../Firebase/firebase.init";
+
+
+initializeAuthentication();
+
+const useFirebase = () => {
+  const [user, setUser] = useState({});
+  const [error, setError] = useState("");
+  // is admin 
+  const [admin, isAdmin]= useState(false);
+  const [isLoading, setIsLoading]= useState(true);
+
+  const auth = getAuth();
+
+  const provider = new GoogleAuthProvider();
+
+  //   google login
+  const signInWithGoogle = (location, history) => {
+ 
+    setIsLoading(true);
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const user = result.user;
+        setUser(user);
+        // handleRegisterUserInfo(user?.email, user?.displayName)
+        const destination = location?.state?.from||'/';
+          history.replace(destination);
+        console.log(result.user);
+      })
+      .catch((error) => {
+        setError(error.message);
+      })
+      .finally(()=>setIsLoading(false));
+  };
+
+  //  logout
+  const handleLogout = () => {
+    setIsLoading(true);
+    signOut(auth)
+      .then(() => {
+        setUser({});
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(()=> setIsLoading(false));
+  };
+
+  //   user registration
+  const handleUserRegister = (email, password, name, location, history) => {
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        setUser(result.user);
+        alert(`Welcome  ${name}, your registration success.`)
+        handleRegisterUserInfo(email, name);
+        const destination = location.state.from||'/';
+        history.replace(destination);
+        handleRegisterUserInfo(email, name);
+        console.log(name);
+      })
+      .catch((error) => {
+        setError(error.message)
+        // alert(error);
+      });
+  };
+
+  //   user email password login
+  const handleUserLogin = (email, password, location, history) => {
+    signInWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+          setUser(result.user);
+          alert("user Logged in");
+          const destination = location.state.from||'/';
+          history.replace(destination);
+        console.log(result.user);
+      })
+      .catch((error) => {
+        setError(error.message);
+        console.log(error.message);
+      });
+  };
+
+  //   on auth stage change
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+      } else {
+        // User is signed out
+        // ...
+      }
+      setIsLoading(false);
+    });
+  }, [auth]);
+
+
+ /*  ================================
+  Collect User Email
+  ================================ */
+  const handleRegisterUserInfo= (email,name)=>{
+    console.log(email)
+    fetch("https://polar-gorge-22890.herokuapp.com/addCustomer", {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify({ email, name }),
+    })
+    .then((res) => res.json())
+    .then((result) => console.log(result));
+  };
+/* ==============================
+ admin Check
+  ============================= */
+
+  useEffect(()=>{
+    fetch(`https://polar-gorge-22890.herokuapp.com/customer/${user?.email}`)
+    .then(res=> res.json())
+    .then(data => isAdmin(data.admin))
+  },[user.email])
+
+  return {
+    isLoading,
+    admin,
+    user,
+    error,
+    signInWithGoogle,
+    handleLogout,
+    handleUserLogin,
+    handleUserRegister,
+  };
+};
+
+
+export default useFirebase;
--- src/components/pages/SingleProduct/SingleProduct.js
+++ src/components/pages/SingleProduct/SingleProduct.js
@@ -1,13 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
 import { useParams } from 'react-router';
+import { Link } from 'react-router-dom';
 import Navigation from '../../shared/Navigation/Navigation';
 
 const SingleProduct = () => {
-    const{id}= useParams();
+    const { id } = useParams();
+    const [product, setProduct] = useState();
+    useEffect(() => {
+        fetch(`http://localhost:5000/product/${id}`)
+            .then(res => res.json())
+            .then(data => setProduct(data));
+    }, [id])
     return (
         <div>
             <Navigation></Navigation>
-            this is single product {id}
+            <div>{product?.title}</div>
+            <div className="my-5">
+                <Container>
+                    <Row xs={1} md={2}>
+                        <Col>
+                            <img src={product?.imgLink} width="60%" alt={product?.title} />
+                        </Col>
+                        <Col>
+                            <div className="p-4">
+                                <div>
+                                    <p className="fs-2 fw-bold">{product?.title}</p>
+                                </div>
+                                <div>
+                                    <Link to={`/product/}`}>
+                                        <button className="order-button mx-2"><h5 style={{ color: "#fff" }}>Add to Cart</h5></button>
+                                    </Link>
+                                    <Link to={`/product/}`}>
+                                        <button className="order-button mx-2"><h5 style={{ color: "#fff" }}>Buy Now</h5></button>
+                                    </Link>
+                                </div>
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
         </div>
     );
 };
